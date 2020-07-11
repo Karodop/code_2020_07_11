9fcecfa9609aad89dc59ecbd0f09144bf48d8ecf
karodop <carolinnee.wit@gmail.com>
2020-07-11 12:46:54 +0200

    zmieniono powitanie vol3

--- src/com/inqoo/basics/HelloWorld.java
+++ src/com/inqoo/basics/HelloWorld.java
@@ -3,7 +3,7 @@ package com.inqoo.basics;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("elowinka:)!");
+        System.out.println("elowinka z bruncha cokolwiek:)!");
 
     }
 
